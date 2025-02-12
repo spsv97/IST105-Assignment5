bd14bfc408c4fc8bea4c0ee01a9e68cba9598689
Steven <steven.sandovalv@gmail.com>
2025-02-11 21:39:46 -0800

    Added instance id

--- form.php
+++ form.php
@@ -4,10 +4,23 @@
     <title>Interactive Treasure Hunt</title>
 </head>
 <body>
-    <h1>Welcome to the Interactive Treasure Hunt!</h1>
-    <p>Instance ID: 
-    <?php echo file_get_contents("instance_id.txt"); ?>
-</p>
+<h1>Welcome to the Interactive Treasure Hunt!</h1>
+<h1>Tu IP es: <span id="ip"></span></h1>
+
+<script>
+    // Utiliza el servicio ipify para obtener la IP pública del cliente
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            // Muestra la IP en el elemento con id="ip"
+            document.getElementById('ip').textContent = data.ip;
+        })
+        .catch(error => {
+            console.error('Error al obtener la IP:', error);
+        });
+    </script>
+    
+
     <form action="process.php" method="POST">
         <label>Number (e.g., birth year):</label>
         <input type="number" name="user_number" required><br><br>
